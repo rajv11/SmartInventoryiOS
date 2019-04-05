4014da99ad088b41710b5edbba2dcf9fb8d4770a
vamshi raj <s531370@nwmissouri.edu>
2019-04-05 11:34:06 -0500

    Removing ignored files

--- Smart Inventory/DerivedData/Smart Inventory/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-Smart Inventory.build/Objects-normal/x86_64/Pods_Smart_Inventory_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/vamshi/Documents/SmartInventoryiOS/Smart\ Inventory/DerivedData/Smart\ Inventory/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-Smart\ Inventory.build/DerivedSources/Pods_Smart_Inventory_vers.c
